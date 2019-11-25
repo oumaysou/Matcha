9ab2cc8755f30edfdce7cc71902423c466359ef8
Kyoya WATANABE <kwatanab@e3r11p18.42.fr>
2019-11-25 14:28:30 +0100

    The token is now updated when user forgots his password

--- back-end/controllers/users/passwordReset.js
+++ back-end/controllers/users/passwordReset.js
@@ -21,17 +21,34 @@ const passwordReset = async (req, res) => {
         whereValue: email
     });
 
-    if (dataPw.affectedRows > 0) {
-        const subject = "Forgot password";
-        const indication = `Your new password is: "${password}". You can sign in here:`;
-        const link = `<a href="http://localhost:3000/">https://www.matcha.com/Signin</a>`;
-        sendMail(email, subject, indication, link);
-        res.send({
-            success: true
-        })
+    let user = await generalQuery.get({table: 'users', field: 'email', value: email});
+    if (user[0]) {
+        const token = await createToken(user[0]);
+        
+        const dataToken = await generalQuery.update({ 
+            table: 'users', 
+            field: 'token', 
+            value: token, 
+            where:'email', 
+            whereValue: email
+        });
+
+        if (dataPw.affectedRows > 0 && dataToken.affectedRows > 0) {
+            const subject = "Forgot password";
+            const indication = `Your new password is: "${password}". You can sign in here:`;
+            const link = `<a href="http://localhost:3000/">https://www.matcha.com/Signin</a>`;
+            sendMail(email, subject, indication, link);
+            res.send({
+                success: true
+            })
+        }
+        else
+            console.error("Something went wrong with the function generalQuery.update().");
     }
-    else
-        console.error("Something went wrong with the function generalQuery.update().");
+    else {
+        console.error("Something went wrong with the function generalQuery.get()");
+    }
+    
 }
 
 module.exports = passwordReset;
